4ce3f40b43fbbe890e8dc9f0e9cb49a1f09be05b
Carla Buongiorno <carlab201103@gmail.com>
2022-01-22 20:22:47 +0000

    Customize admin

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,32 @@ from .models import Product, Category
 
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Admin setting to display a list of products
+    """
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Admin setting to display a list of categories
+    """
+    list_display = (
+        'friendly_name',
+        'backend_name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
