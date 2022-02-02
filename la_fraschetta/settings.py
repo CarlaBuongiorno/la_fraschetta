d76abbb308820ab8ec5d711ca5bb23ce10c0951a
Carla Buongiorno <carlab201103@gmail.com>
2022-02-02 21:00:43 +0000

    feat: Add views and checkout template to render crispy form

--- la_fraschetta/settings.py
+++ la_fraschetta/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'la_fraschetta.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,6 +82,10 @@ TEMPLATES = [
                 'bag.contexts.bag_contents',
                 'products.contexts.all_categories',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
