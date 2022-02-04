1c1c4c3a89bce83d30bdc11f2498b72aa846be5f
Carla Buongiorno <carlab201103@gmail.com>
2022-02-04 17:15:55 +0000

    feat: Add Stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -5,6 +5,9 @@ from .forms import OrderForm
 
 
 def checkout(request):
+    """
+    View to display checkout page
+    """
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -14,6 +17,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51K3djBIxKeITzM6mvn1ZJbg45EqEwk3jFE8k5o09TL816IyG0cLjlhv9D73JDN5DO0DU6fdvm7nFT8muSkKWs26l00IB20tpRm',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
