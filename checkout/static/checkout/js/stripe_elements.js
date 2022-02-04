23d102123b9079739e1eba16a6fff8eca939fe57
Carla Buongiorno <carlab201103@gmail.com>
2022-02-04 03:44:38 +0000

    style: Add a muted green to all muted text

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,28 @@
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+    CSS from here: 
+    https://stripe.com/docs/stripe-js
+*/
+
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#0f5520',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#990b16',
+        iconColor: '#990b16'
+    }
+};
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
