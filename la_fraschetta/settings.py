9c4900cb3e57d89d21399b97baedb02085d4b9a2
Carla Buongiorno <carlab201103@gmail.com>
2022-02-07 20:50:50 +0000

    feat: Add confirmation emails

--- la_fraschetta/settings.py
+++ la_fraschetta/settings.py
@@ -182,3 +182,4 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'lafraschetta@example.com'
