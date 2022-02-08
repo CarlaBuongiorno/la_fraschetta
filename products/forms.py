42e85f8a7345de23d8963185228d6db5ca951092
Carla Buongiorno <carlab201103@gmail.com>
2022-02-08 08:15:45 +0000

    feat: Add Product form for Admin

---
+++ products/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+    """
+    Set up model form to add/edit a product
+    """
+    class Meta:
+        """Set fields from product model"""
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        """
+        Set placeholders and labels for fields in the form
+        """
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-green'
