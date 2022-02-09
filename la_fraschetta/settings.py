199316f16136770f750da654a5d178e7ee69a937
Carla Buongiorno <carlab201103@gmail.com>
2022-02-09 17:47:58 +0000

    wip: Remove secret key and set debug

--- la_fraschetta/settings.py
+++ la_fraschetta/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ee-=x794mqvm(x5jf+uh-v-fij8@c^9ula=b%@a6wj%*v4nihs'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['la-fraschetta.herokuapp.com', 'localhost']
 
