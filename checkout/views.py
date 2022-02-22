eac51bf6627f8d2ee0cccde9cafb7ce390d4be26
Carla Buongiorno <carlab201103@gmail.com>
2022-02-22 10:08:27 +0000

    wip: Change max length of order number in Order model to be 27 instead of 32

--- checkout/views.py
+++ checkout/views.py
@@ -170,7 +170,8 @@ def checkout_success(request, order_number):
                 user_profile_form.save()
 
     messages.success(request, f'Order successfully processed! \
-        Your order number is {order_number}. A confirmation email \
+        Your order number is \
+        {order_number}. A confirmation email \
         will be sent to {order.email}.')
 
     if 'bag' in request.session:
