21044655ced84eaab8cbbaa75b142ae5206224bb
Carla Buongiorno <carlab201103@gmail.com>
2022-02-25 19:13:57 +0000

    wip: Testing debug

--- la_fraschetta/settings.py
+++ la_fraschetta/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get("DEBUG") == "False":
+if os.environ.get("DEBUG") is False:
     DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['la-fraschetta.herokuapp.com', 'localhost']
