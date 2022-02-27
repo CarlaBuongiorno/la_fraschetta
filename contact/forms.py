a3b83bb77f4bf096774415756f1ce44a20037855
Carla Buongiorno <carlab201103@gmail.com>
2022-02-27 22:12:09 +0000

    style: Style the contact form

--- contact/forms.py
+++ contact/forms.py
@@ -17,3 +17,27 @@ class ContactForm(forms.Form):
                               max_length=300,
                               widget=forms.Textarea(),
                               label='Message')
+
+    def __init__(self, *args, **kwargs):
+        """
+            Add placeholders and classes, remove auto-generated
+            labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Name',
+            'email': 'Email Address',
+            'message': 'Your Message',
+            'subject': 'Subject',
+        }
+
+        self.fields['name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-green green \
+                contact-form-input'
+            self.fields[field].label = False
