3cd73f54d57fe879c18a9c1142778eded8b4d632
Carla Buongiorno <carlab201103@gmail.com>
2026-09-24 11:16:39 +0200

    Modify build.sh file

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-set -o errexit
- pip install -r requirements.txt
- python manage.py collectstatic --noinput
- python manage.py makemigrations && python manage.py migrate
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+
+python manage.py migrate
